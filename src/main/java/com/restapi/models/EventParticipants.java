a9c81b705fc1ecaf0fc606cf4abfb887576e2af5
Petko Todorov <petkoptodorov@gmail.com>
2024-01-10 21:23:27 +0200

    add event_participants

---
+++ src/main/java/com/restapi/models/EventParticipants.java
@@ -0,0 +1,54 @@
+package com.restapi.models;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="event_participants")
+public class EventParticipants {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToMany(cascade = {CascadeType.DETACH,
+            CascadeType.MERGE,
+            CascadeType.REFRESH,
+            CascadeType.REMOVE})
+    private List<Games> games = new ArrayList<>();
+
+    @ManyToMany(cascade = {CascadeType.DETACH,
+            CascadeType.MERGE,
+            CascadeType.REFRESH,
+            CascadeType.REMOVE})
+    private List<User> users = new ArrayList<>();
+
+    public EventParticipants() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Games> getGames() {
+        return games;
+    }
+
+    public void setGames(List<Games> games) {
+        this.games = games;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
