43cc440f93501f4345deea98ade480acc6f90cbc
Petko Todorov <petkoptodorov@gmail.com>
2024-01-11 18:00:31 +0200

    add dto

---
+++ src/main/java/com/restapi/dto/EventsDto.java
@@ -0,0 +1,9 @@
+package com.restapi.dto;
+
+import com.restapi.models.User;
+
+import java.util.Date;
+import java.util.List;
+
+public record EventsDto(int id, Date date, String description, List<User> users) {
+}
